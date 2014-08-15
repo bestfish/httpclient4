6ed707788bd481a395add670dbb290d4b9350815
bestfish <bestfish@sina.cn>
2014-08-15 23:04:53 +0800

    daemon thread factory

---
+++ src/main/java/com/fish/play/http/poolfactory/DaemonThreadFactory.java
@@ -0,0 +1,28 @@
+package com.fish.play.http.poolfactory;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DaemonThreadFactory implements ThreadFactory {
+	static final AtomicInteger poolNumber = new AtomicInteger(1);
+	final ThreadGroup group;
+	final AtomicInteger threadNumber = new AtomicInteger(1);
+	final String namePrefix;
+
+	DaemonThreadFactory() {
+		SecurityManager s = System.getSecurityManager();
+		group = (s != null) ? s.getThreadGroup() : Thread.currentThread()
+				.getThreadGroup();
+		namePrefix = "pool-" + poolNumber.getAndIncrement() + "-thread-";
+	}
+
+	public Thread newThread(Runnable r) {
+		Thread t = new Thread(group, r, namePrefix
+				+ threadNumber.getAndIncrement(), 0);
+		t.setDaemon(true);
+		if (t.getPriority() != Thread.NORM_PRIORITY) {
+			t.setPriority(Thread.NORM_PRIORITY);
+		}
+		return t;
+	}
+}
